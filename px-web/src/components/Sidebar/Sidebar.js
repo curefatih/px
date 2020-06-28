0d631ea1c43869e82f2995fcd2851954e75ba775
curefatih <curefat@gmail.com>
2020-06-28 04:04:38 +0300

    update sidebar with link

--- px-web/src/components/Sidebar/Sidebar.js
+++ px-web/src/components/Sidebar/Sidebar.js
@@ -1,15 +1,18 @@
 import React from 'react';
 
 import "./Sidebar.scss";
+import { Link } from 'react-router-dom';
 
 const Sidebar = (props) => {
 
   return (
     <div className="sidebar xl-center">
 
+      <Link to="/">
         <div className="logo icon">
           <props.logo />
         </div>
+      </Link>
 
     </div>
   );
